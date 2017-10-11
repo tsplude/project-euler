65cdd4a7c801ee36702fa96b2b977aceef91d323
Tyler Plude <tsplude@gmail.com>
2017-10-11 17:00:49 -0400

    Problem 001 solution

--- problem-001/problem-001.go
+++ problem-001/problem-001.go
@@ -3,5 +3,20 @@ package main
 import "fmt"
 
 func main() {
-     fmt.Printf("hello, tyler\n")
+
+     // Collect all positive integers < 1000 that are divided by 3 or 5
+     var divisors []int
+     for i := 1; i < 1000; i += 1 {
+     	 if i % 3 == 0 || i % 5 == 0 {
+	    divisors = append(divisors, i)
+	 }
+     }
+
+     // Find the sum of the divisors
+     sum := 0
+     for i := 0; i < len(divisors); i += 1 {
+     	 sum += divisors[i]
+     }
+
+     fmt.Println("Answer:", sum)
 }

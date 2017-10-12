356900275d463d0bb665bea3892d65db909f4842
Tyler Plude <tsplude@gmail.com>
2017-10-12 13:07:41 -0400

    Renaming files and solution to p003

---
+++ p003/p003.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+   "fmt"
+   "math"
+)
+
+func check_prime(x int, divs []int) bool {
+     for i := 0; i < len(divs); i += 1 {
+     	 if x % divs[i] == 0 {
+	    return false
+	 }
+     }
+     return true
+}
+
+func factors(x int) []int {
+     var divs []int
+     for i := 2; i < int(math.Sqrt(float64(x))); i += 1 {
+     	 if x % i == 0 && check_prime(i, divs) {
+	    divs = append(divs, i)
+	 }
+     }
+     return divs
+}
+
+func main() {
+
+     // Find factors
+     divs := factors(600851475143)
+
+     fmt.Println("Answer:", divs[len(divs)-1])
+}
